f26857fae9d853c7600689ae5a24bf2a6a2b2a63
Michael Carroll <juwara0@users.noreply.github.com>
2018-04-04 12:13:50 -0500

    convert missed file to ember-decorators

--- tests/dummy/app/pods/link/controller.js
+++ tests/dummy/app/pods/link/controller.js
@@ -1,7 +1,7 @@
 import { inject as service } from '@ember/service';
 import { htmlSafe } from '@ember/string';
 import Controller from '@ember/controller';
-import computed, {readOnly} from 'ember-computed-decorators'
+import {computed, readOnly} from 'ember-decorators/object'
 
 export default Controller.extend({
   notifications: service('notification-messages'),
@@ -19,8 +19,8 @@ export default Controller.extend({
 
   @readOnly
   @computed('fontSize')
-  fontSizeStyle (fontSize) {
-    return htmlSafe(`font-size: ${fontSize}px`);
+  get fontSizeStyle () {
+    return htmlSafe(`font-size: ${this.get('fontSize')}px`);
   },
 
   actions: {
